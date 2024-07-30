0c9f0e5e9a3511a11b57c1ee443c18debd76276d
jeanluca <floresjeanluca@gmail.com>
2024-07-29 21:28:41 -0600

    taller1-Documento

---
+++ taller1-cf190725/ejercicio1/MVCProducto/MVCProducto/Controllers/ProductoController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCProducto.Models;
+
+namespace MVCProducto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ProductosDBContext _context;
+
+        public ProductoController(ProductosDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Producto
+        public async Task<IActionResult> Index()
+        {
+            var productosDBContext = _context.Producto.Include(p => p.Categoria);
+            return View(await productosDBContext.ToListAsync());
+        }
+
+        // GET: Producto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Producto
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public IActionResult Create()
+        {
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categoria, "Id", "Id");
+            return View();
+        }
+
+        // POST: Producto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,NombreProducto,Precio,Stock,CodigoCategoria")] Producto producto)
+        {
+            //Removemos la validacion de la propiedad de navegacion de genero
+            ModelState.Remove("Categoria");
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categoria, "Id", "Id", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Producto.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categoria, "Id", "Id", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,NombreProducto,Precio,Stock,CodigoCategoria")] Producto producto)
+        {
+            if (id != producto.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categoria, "Id", "Id", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Producto
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producto = await _context.Producto.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Producto.Remove(producto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return _context.Producto.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ taller1-cf190725/ejercicio2/Inventario.API/Inventario.API/Program.cs
@@ -0,0 +1,49 @@
+using Inventario.BL.Interfaces;
+using Inventario.BL.Services;
+using Inventario.Common;
+using Inventario.DAL;
+using AutoMapper;
+using Inventario.BL.Automapper;
+using Inventario.DAL.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Otras configuraciones de servicios
+
+// Registrar IProductoService
+//builder.Services.AddScoped<IProductoService, ProductoService>();
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddServiceConnector();
+
+/*
+builder.Services.AddTransient<IProductoRepository, ProductoRepository>();
+builder.Services.AddTransient<IDatabaseRepository, DatabaseRepository>();
+builder.Services.AddAutoMapper(typeof(AutomapperProfile));
+*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
